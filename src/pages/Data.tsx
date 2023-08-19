4144564d5038919afd0f560de4f554525ce43d5c
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-19 18:06:15 +0530

    replace state with var for getting localstorage details

--- src/pages/Data.tsx
+++ src/pages/Data.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Grid from "../components/Grid";
@@ -6,9 +6,7 @@ import DeptList from "../components/DeptList";
 
 const Data = () => {
   const navigate = useNavigate();
-  const [userDetails, setUserDetails] = useState(
-    JSON.parse(localStorage.getItem("userDetails") || "null")
-  );
+  const userDetails = JSON.parse(localStorage.getItem("userDetails") || "null");
 
   useEffect(() => {
     if (!userDetails) {
